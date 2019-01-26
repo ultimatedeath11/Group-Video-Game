93bb031d8ae65e6989190663410eb2804b04c1dd
Noah Franck <franck.noahk@gmal.com>
2019-01-26 01:43:28 -0600

    Added comments to the classes above the decloration

--- src/Creature.java
+++ src/Creature.java
@@ -1,4 +1,5 @@
-
+/*General idea of the Creature is that anything that has stats or can deal damage will be a creature. If it is a monster, player, or npc they should be some form of creature I believe.
+*/
 public class Creature() extends Sprite{
 
 
--- src/Sprite.java
+++ src/Sprite.java
@@ -1,3 +1,7 @@
+/*
+Sprites should be anything that moves. If it exists on the map and is able to move our goal should be to make it be a sprite object. Implamentation will be tricky especially with movement, but we can do this!!!
+*/
+
 public class Sprite{
 
     //Setting up Contstructor
